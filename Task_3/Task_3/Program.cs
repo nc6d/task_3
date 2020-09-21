d1f282749be0edb263dd53c9c88b4d6d9464d1bc
nc6d <6od92002@gmail.com>
2020-09-21 20:36:48 +0300

    commit 1

---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool check = true;
+
+            while (check == true)
+            {
+                try
+                {
+                    check = false;
+
+                    Console.WriteLine("Enter a: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter b: ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter c: ");
+                    long c = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine($" Number A: {a}\n Number B: {b}\n Number C: {c}");
+
+                }
+                catch
+                {
+                    Console.WriteLine("Please, enter correct values!");
+                    check = true;
+                }
+            }
+
+
+        }
+    }
+}
